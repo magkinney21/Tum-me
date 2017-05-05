506a81f1583db0fb7ade7b29460da9b3466cc43d
Magkinney21 <magkinney21@gmail.com>
2017-05-05 15:23:52 -0400

    posts controller set up

--- app/controllers/api/post_controller.rb
+++ app/controllers/api/post_controller.rb
@@ -1,14 +1,39 @@
-class PostsController < ApplicationController
+class Api::PostsController < ApplicationController
 
   def index
     @posts = Post.all
     render json: @posts
   end
 
+  def show
+    @post = current_user.post.find(parmas[:id])
+    render json: @post
+  end
+
+
   def create
+    @post = Post.new(post_params)
+      if @post.save
+        render json: @post, status: :created
+      else
+        render json: @post.errors, status: :unprocessable_entity
+    end
   end
 
   def update
+    @post = current_user.posts.find(params[:id])
+
+  if @post.update(post_params)
+    render json: @post, status: :updated
+  else
+    render json: @post.errors , status: :unprocessable_entity
   end
 end
 
+private
+def post_params
+  params.require(:posts)
+      .permit(:title, :content)
+      .merge(user_id: current_user.id)
+    end
+end
